01a3841008724199fd6bc9b3ea3e8e38b6410b8c
YUTARO KONDA <kondayutarou@gmail.com>
2022-07-19 21:29:54 +0900

    Initial commit

---
+++ fakeapis/index.js
@@ -0,0 +1,7 @@
+const participationDateList = require('./ParticipationDateList.json');
+// Something more
+
+module.exports = () => ({
+  participationDateList: participationDateList
+  // Something more
+});
