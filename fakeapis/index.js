db5e841357a17a3140454b78a277a44041db099a
YUTARO KONDA <kondayutarou@gmail.com>
2022-07-24 10:00:59 +0900

    Add pickupPoints

--- fakeapis/index.js
+++ fakeapis/index.js
@@ -1,7 +1,9 @@
 const participationDateList = require('./ParticipationDateList.json');
+const pickupPoints = require('./PickupPoints.json');
 // Something more
 
 module.exports = () => ({
-  participationDateList: participationDateList
+  participationDateList: participationDateList,
+  pickupPoints: pickupPoints
   // Something more
 });
